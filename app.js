a74131a4eccc215ebeca53cc40978058d1054b39
uanoob <usnet@ukr.net>
2018-06-08 15:48:24 +0300

    Fetch data from local json file

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-document.getElementById('button1').addEventListener('click', getText);
+// document.getElementById('button1').addEventListener('click', getText);
 
-function getText() {
-  fetch('test.txt')
+// Get local text file
+
+// function getText() {
+//   fetch('test.txt')
+//     .then(function(response) {
+//       return response.text();
+//     })
+//     .then(function(data) {
+//       document.getElementById('output').innerHTML = data;
+//     })
+//     .catch(function(error) {
+//       console.log(error);
+//     });
+// }
+
+document.getElementById('button2').addEventListener('click', getJson);
+
+// Get local json data
+
+function getJson() {
+  fetch('posts.json')
     .then(function(response) {
-      return response.text();
+      return response.json();
     })
     .then(function(data) {
-      document.getElementById('output').innerHTML = data;
-    })
-    .catch(function(error) {
-      console.log(error);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      });
+
+      document.getElementById('output').innerHTML = output;
     });
 }
