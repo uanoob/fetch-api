ee2bee6da9cf62ee102e241461c560a2e0a6efca
uanoob <usnet@ukr.net>
2018-06-08 16:06:32 +0300

    Fetch data from external api

--- app.js
+++ app.js
@@ -15,19 +15,38 @@
 //     });
 // }
 
-document.getElementById('button2').addEventListener('click', getJson);
+// document.getElementById('button2').addEventListener('click', getJson);
 
 // Get local json data
 
-function getJson() {
-  fetch('posts.json')
+// function getJson() {
+//   fetch('posts.json')
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(data) {
+//       let output = '';
+//       data.forEach(function(post) {
+//         output += `<li>${post.title}</li>`;
+//       });
+
+//       document.getElementById('output').innerHTML = output;
+//     });
+// }
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// Get from external API
+
+function getExternal() {
+  fetch('https://api.github.com/users')
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
       let output = '';
-      data.forEach(function(post) {
-        output += `<li>${post.title}</li>`;
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
       });
 
       document.getElementById('output').innerHTML = output;
